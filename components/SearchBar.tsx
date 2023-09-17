ea30fce7c27dad4a2d80dc1cddae68ac16e00946
unknown <fefe.borrometi@gmail.com>
2023-09-18 00:51:40 +0200

    chore: searchbar with headlessui

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,11 +1,22 @@
 "use client";
 
+import { useState } from "react";
+
+import SearchManufacturer from "./SearchManufacturer";
+
 const SearchBar = () => {
+  const [manufacturer, setManufacturer] = useState("");
+
   const handleSearch = () => {};
 
   return (
     <form className="searchbar" onSubmit={handleSearch}>
-      <div className="searchbar__item"></div>
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+      </div>
     </form>
   );
 };
