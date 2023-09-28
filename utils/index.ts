97d68e5150644f7b5e7bda86869abd830ccd2f03
unknown <fefe.borrometi@gmail.com>
2023-09-28 15:35:54 +0200

    chore: dynamic car images

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars() {
   const headers = {
     "X-RapidAPI-Key": "be0bd51ad9msh7725a041b11e18ep17c2f1jsn184b0efc1aa0",
@@ -13,6 +15,21 @@ export async function fetchCars() {
 
   return result;
 }
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+
+  const { make, year, model } = car;
+
+  url.searchParams.append("customer", "hrjavascript-mastery");
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  url.searchParams.append("angle", `${year}`);
+
+  return `${url}`;
+};
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
   const basePricePerDay = 50; // Base rental price per day in dollars
